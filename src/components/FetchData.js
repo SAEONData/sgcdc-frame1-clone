92ac1cb32e85512352e9f5e26a123eb64d57bad3
dcugh <diana@gmail.com>
2019-06-10 14:14:08 +0200

    feat: map content to footer sections

---
+++ src/components/FetchData.js
@@ -0,0 +1,23 @@
+import React, { Suspense } from 'react'
+import { useFetch } from 'react-hooks-fetch'
+
+const Err = ({ error }) => <span>Error:{error.message}</span>
+
+const FetchData = () => {
+  const url = 'http://sgcdc.saeon.ac.za:8081/spreadsheet'
+  const { error, data } = useFetch(url)
+  if (error) return <Err error={error} />
+  if (!data) return null
+  console.log(data)
+  
+  return (
+    <Suspense fallback={<span>loading..</span>}>
+       <span> {data.map((data) => data.title)} </span>
+    </Suspense>
+   
+  )
+
+}
+
+
+export default FetchData
